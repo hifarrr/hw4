93861c726f61eab17bdd83e209533ffb244ba01e
yyyyyuan <31830607+yyyyyuan@users.noreply.github.com>
2017-10-01 03:02:54 -0700

    Create lewitt-413.js

---
+++ lewitt-413.js
@@ -0,0 +1,14 @@
+background(255);
+while(true){
+	var x1=random(width)/4; y1=random(width)/4;a=random(width)/4;
+  var h=random(360); h2=random(360);h3=random(360);h4=random(360);
+	colorMode(HSB);
+  fill(h,20,100);
+	rect(x1,y1,a,a);
+  fill(h2,20,100);
+	rect(x1+a,y1,a,a);
+  fill(h3,20,100);
+	rect(x1,y1+a,a,a);
+  fill(h4,20,100);
+	rect(x1+a,y1+a,a,a);
+}
