876075c8e00a8e1406fda2418015461e9a9e3df5
yyyyyuan <31830607+yyyyyuan@users.noreply.github.com>
2017-10-01 18:53:08 -0700

    Create lewitt-160.js

---
+++ lewitt-160.js
@@ -0,0 +1,11 @@
+while(true){
+  var x=mouseX;x2=random(width);x3=random(width);
+  background(255)
+	noFill();
+  stroke(0);
+  rectMode(CENTER);
+	rect(width/2, height/2, x, x);
+  stroke(255,0,0);
+  line(width/2-x2,height/2-x2,width/2+x2,height/2+x2);
+  line(width/2+x3,height/2-x3,width/2-x3,width/2+x3);
+}
