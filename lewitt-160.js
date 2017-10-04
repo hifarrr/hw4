6cb3b19b4e2fb2a5c5c624720329ca76c4b29238
yyyyyuan <31830607+yyyyyuan@users.noreply.github.com>
2017-10-04 08:40:15 -0700

    Update lewitt-160.js

--- lewitt-160.js
+++ lewitt-160.js
@@ -1,10 +1,11 @@
+
 while(true){
-  var x=mouseX;x2=random(width);x3=random(width);
+  var a=random(width); x=mouseX;x2=random(width);x3=random(width);
   background(255)
 	noFill();
   stroke(0);
-  rectMode(CENTER);
-	rect(width/2, height/2, x, x);
+  var b =random(width-a);
+	rect(a, a, b, b);
   stroke(255,0,0);
   line(width/2-x2,height/2-x2,width/2+x2,height/2+x2);
   line(width/2+x3,height/2-x3,width/2-x3,width/2+x3);
